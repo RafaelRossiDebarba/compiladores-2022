ce37b103988228d0d899da5f51a870dacf67b876
Rafael Rossi Debarba <rafadeross@hotmail.com>
2022-09-04 16:57:06 -0300

    change in reading type

--- lexico.c
+++ lexico.c
@@ -1,477 +1,304 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define TAM 255
-#define DEBUG 0
+#define DEBUG 1
 
-void number_start(char word[TAM]);
-void number_q0(int counter, char word[TAM]);
-void number_q1(int counter, char word[TAM]);
-void number_q2(int counter, char word[TAM]);
-void number_q3(int counter, char word[TAM]);
-void number_q4(int counter, char word[TAM]);
+#define CODE_NUMBER 1
+#define CODE_BIN 2
+#define CODE_HEX 3
+#define CODE_OPEN_KEYS 4  // {
+#define CODE_CLOSE_KEYS 5 // }
+
+void read_file();
+void add_current_in_word();
+void print_word();
+
+void number_q0();
+void number_q1();
+void number_q2();
+void number_q3();
+void number_q4();
 
 void err();
 void finish(char word[TAM]);
 
-void bin_start(char word[TAM]);
-void bin_q0(int counter, char word[TAM]);
-void bin_q1(int counter, char word[TAM]);
-void bin_q2(int counter, char word[TAM]);
+void bin_q1();
 
-void hex_start(char word[TAM]);
-void hex_q0(int counter, char word[TAM]);
-void hex_q1(int counter, char word[TAM]);
-void hex_q2(int counter, char word[TAM]);
+void hex_q1();
 
-int NEXT = 1;
+char CURRENT, NEXT;
+char WORD[TAM];
+int POSITION;
+FILE *PONT_ARQ;
+int CONTINUE = 1;
+int LINE = 0;
 
-int main() {
-  FILE *pont_arq;
-  char c, word[TAM], *aux;
-  int position;
+int main()
+{
+  char *aux;
 
-  pont_arq = fopen("numeros.txt","r");
-  if (pont_arq == NULL)
+  PONT_ARQ = fopen("numeros.txt", "r");
+  if (PONT_ARQ == NULL)
   {
     printf("Erro ao tentar abrir o arquivo!");
     return EXIT_FAILURE;
   }
 
-  do {
-    memset(word, 0, sizeof word);
-    position = 0;
-    int check = 0;
-    do {
-      c = fgetc(pont_arq);
-      if(c == EOF) {
-        break;
+  NEXT = ' ';
+
+  do
+  {
+    if (CONTINUE == 0)
+    {
+      break;
+    }
+    // memset(WORD, 0, sizeof WORD);
+    for (int i = 0; i < TAM; i++)
+    {
+      WORD[i] = '\0';
+    }
+    POSITION = 0;
+
+    read_file();
+    if (CURRENT == EOF || CURRENT == '\0')
+    {
+      break;
+    }
+    if (isalpha(CURRENT))
+    {
+      printf("alf\n");
+    }
+    else if (CURRENT == '\n')
+    {
+      LINE++;
+    }
+    else if (CURRENT == '{')
+    {
+      printf("%d\n", CODE_OPEN_KEYS);
+    }
+    else if (CURRENT == '}')
+    {
+      printf("%d\n", CODE_CLOSE_KEYS);
+    }
+    else if (CURRENT == '0')
+    {
+      if (NEXT == 'b')
+      {
+        WORD[POSITION] = '0';
+        WORD[POSITION + 1] = 'b';
+        POSITION += 2;
+        read_file();
+        bin_q1();
       }
-      aux = &c;
-      if(c != '\n' && c != ' ' && c != ';') {
-        word[position] = c;
-        position++;
+      else if (NEXT == 'x')
+      {
+        WORD[POSITION] = '0';
+        WORD[POSITION + 1] = 'x';
+        POSITION += 2;
+        read_file();
+        hex_q1();
       }
-
-      if(c == '\n') {
-        check = 1;
+      else
+      {
+        add_current_in_word();
+        number_q1();
       }
-
-    } while(check == 0);
-
-    if(DEBUG)
-      printf("---------\n");
-    
-    hex_start(word);
-    if(NEXT)
-      bin_start(word);
-    if(NEXT)
-      number_start(word);
-    
-    if(NEXT) {
-      printf("não reconhecido ");
-      for(int i = 0; i < TAM; i++) {
-        printf("%c", word[i]);
-      }
-      printf("\n");
     }
-    
-    NEXT = 1;
+    else if (CURRENT == '1' || CURRENT == '2' || CURRENT == '3' || CURRENT == '4' || CURRENT == '5' ||
+             CURRENT == '6' || CURRENT == '7' || CURRENT == '8' || CURRENT == '9' || CURRENT == '+' ||
+             CURRENT == '-' )
+    {
+      add_current_in_word();
+      number_q1();
+    }
+    else if (CURRENT == '.')
+    {
+      add_current_in_word();
+      number_q2();
+    }
+  } while (NEXT != EOF && CONTINUE);
 
-  } while (c != EOF);
-
-  return(0);
+  return (0);
 }
 
-void number_start(char word[TAM]) {
-  int counter = 0;
-  number_q0(counter, word);
+void read_file()
+{
+  CURRENT = NEXT;
+  NEXT = fgetc(PONT_ARQ);
+  if (NEXT == EOF)
+    CONTINUE = 0;
 }
 
-void number_q0(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '1') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '2') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '3') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '4') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '5') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '6') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '7') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '8') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '9') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '.') {
-      number_q2( ++counter, word);
-    }
-    else if(word[counter] == '+') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '-') {
-      number_q1( ++counter, word);
-    }
-    else if(!word[counter]) {
-      finish(word);
-    }
-    else {
-      if(DEBUG)
-        printf("number_q0 %c\n", word[counter]);
-      err();
-    }
+void add_current_in_word()
+{
+  WORD[POSITION] = CURRENT;
+  POSITION++;
+}
+
+void number_q1()
+{
+  read_file();
+  if (CURRENT == '0' || CURRENT == '1' || CURRENT == '2' || CURRENT == '3' ||
+      CURRENT == '4' || CURRENT == '4' || CURRENT == '5' || CURRENT == '6' ||
+      CURRENT == '7' || CURRENT == '8' || CURRENT == '9')
+  {
+    add_current_in_word();
+    number_q1();
+  }
+  else if (CURRENT == '.')
+  {
+    add_current_in_word();
+    number_q3();
+  }
+  else if (CURRENT == 'e')
+  {
+    add_current_in_word();
+    number_q4();
+  }
+  else if (CURRENT == ' ' || CURRENT == ';' || CURRENT == ',')
+  {
+    printf("%d\n", CODE_NUMBER);
+  }
+  else
+  {
+    if (DEBUG)
+      printf("number_q1\n");
+    err();
   }
 }
 
-void number_q1(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '1') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '2') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '3') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '4') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '5') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '6') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '7') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '8') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '9') {
-      number_q1( ++counter, word);
-    }
-    else if(word[counter] == '.') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == 'e') {
-      number_q4( ++counter, word);
-    }
-    else if(!word[counter]) {
-      finish(word);
-    }
-    else {
-      if(DEBUG)
-        printf("number_q1 %c\n", word[counter]);
-      err();
-    }
+void number_q2()
+{
+  read_file();
+  if (CURRENT == '0' || CURRENT == '1' || CURRENT == '2' || CURRENT == '3' ||
+      CURRENT == '4' || CURRENT == '4' || CURRENT == '5' || CURRENT == '6' ||
+      CURRENT == '7' || CURRENT == '8' || CURRENT == '9')
+  {
+    add_current_in_word();
+    number_q3();
+  }
+  else if (CURRENT == 'e')
+  {
+    add_current_in_word();
+    number_q4();
+  }
+  else
+  {
+    if (DEBUG)
+      printf("number_q2\n");
+    err();
   }
 }
 
-void number_q2(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '1') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '2') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '3') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '4') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '5') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '6') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '7') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '8') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '9') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == 'e') {
-      number_q4( ++counter, word);
-    }
-    else {
-      if(DEBUG)
-        printf("number_q2 %c\n", word[counter]);
-      err();
-    }
+void number_q3()
+{
+  read_file();
+  if (CURRENT == '0' || CURRENT == '1' || CURRENT == '2' || CURRENT == '3' ||
+      CURRENT == '4' || CURRENT == '4' || CURRENT == '5' || CURRENT == '6' ||
+      CURRENT == '7' || CURRENT == '8' || CURRENT == '9')
+  {
+    add_current_in_word();
+    number_q3();
+  }
+  else if (CURRENT == 'e')
+  {
+    number_q4();
+  }
+  else if (CURRENT == ' ' || CURRENT == ';' || CURRENT == ',')
+  {
+    printf("%d\n", CODE_NUMBER);
+  }
+  else
+  {
+    if (DEBUG)
+      printf("number_q3\n");
+    err();
   }
 }
 
-void number_q3(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '1') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '2') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '3') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '4') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '5') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '6') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '7') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '8') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == '9') {
-      number_q3( ++counter, word);
-    }
-    else if(word[counter] == 'e') {
-      number_q4( ++counter, word);
-    }
-    else if(!word[counter]) {
-      finish(word);
-    }
-    else {
-      if(DEBUG)
-        printf("number_q3 %c\n", word[counter]);
-      err();
-    }
+void number_q4()
+{
+  read_file();
+  if (CURRENT == '0' || CURRENT == '1' || CURRENT == '2' || CURRENT == '3' ||
+      CURRENT == '4' || CURRENT == '4' || CURRENT == '5' || CURRENT == '6' ||
+      CURRENT == '7' || CURRENT == '8' || CURRENT == '9')
+  {
+    add_current_in_word();
+    number_q4();
+  }
+  else if (CURRENT == ' ' || CURRENT == ';' || CURRENT == ',')
+  {
+    printf("%d\n", CODE_NUMBER);
+  }
+  else
+  {
+    if (DEBUG)
+      printf("number_q4\n");
+    err();
   }
 }
 
-void number_q4(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '1') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '2') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '3') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '4') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '5') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '6') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '7') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '8') {
-      number_q4( ++counter, word);
-    }
-    else if(word[counter] == '9') {
-      number_q4( ++counter, word);
-    }
-    else if(!word[counter]) {
-      finish(word);
-    }
-    else {
-      if(DEBUG)
-        printf("number_q4 %c\n", word[counter]);
-      err();
-    }
+void bin_q1()
+{
+  read_file();
+  if (CURRENT == '0' || CURRENT == '1')
+  {
+    add_current_in_word();
+    bin_q1();
+  }
+  else if (CURRENT == ' ' || CURRENT == ';' || CURRENT == ',')
+  {
+    printf("%d\n", CODE_BIN);
+  }
+  else
+  {
+    if (DEBUG)
+      printf("bin_q1 CURRENT %s NEXT %s\n", CURRENT, NEXT);
+    err();
   }
 }
 
-void bin_start(char word[TAM]) {
-  int counter = 0;
-  bin_q0(counter, word);
-}
-
-void bin_q0(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      bin_q1( ++counter, word);
-    }
-    else {
-      if(DEBUG)
-        printf("bin_q0 %c\n", word[counter]);
-      err();
-    }
+void hex_q1()
+{
+  read_file();
+  if (CURRENT == '0' || CURRENT == '1' || CURRENT == '2' || CURRENT == '3' || CURRENT == '4' || CURRENT == '5' ||
+      CURRENT == '6' || CURRENT == '7' || CURRENT == '8' || CURRENT == '9' || CURRENT == 'a' || CURRENT == 'b' ||
+      CURRENT == 'c' || CURRENT == 'd' || CURRENT == 'e' || CURRENT == 'f')
+  {
+    add_current_in_word();
+    hex_q1();
+  }
+  else if (CURRENT == ' ' || CURRENT == ';' || CURRENT == ',')
+  {
+    printf("%d\n", CODE_HEX);
+  }
+  else
+  {
+    if (DEBUG)
+      printf("hex_q1\n");
+    err();
   }
 }
 
-void bin_q1(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == 'b') {
-      bin_q2( ++counter, word);
-    }
-    else {
-      if(DEBUG)
-        printf("bin_q1 %c\n", word[counter]);
-      err();
-    }
+void err()
+{
+  printf("ERR\n");
+  CONTINUE = 0;
+  if (DEBUG)
+  {
+    print_word();
+    printf("Current %c NEXT %c\n", CURRENT, NEXT);
   }
 }
 
-void bin_q2(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      bin_q2( ++counter, word);
-    }
-    else if(word[counter] == '1') {
-      bin_q2( ++counter, word);
-    }
-    else if(!word[counter]) {
-      finish(word);
-    }
-    else {
-      if(DEBUG)
-        printf("bin_q2 %c\n", word[counter]);
-      err();
-    }
-  }
-}
-
-void hex_start(char word[TAM]) {
-  int counter = 0;
-  hex_q0(counter, word);
-}
-
-void hex_q0(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      hex_q1( ++counter, word);
-    }
-    else {
-      if(DEBUG)
-        printf("hex_q0 %c\n", word[counter]);
-      err();
-    }
-  }
-}
-
-void hex_q1(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == 'x') {
-      hex_q2( ++counter, word);
-    }
-    else {
-      if(DEBUG)
-        printf("hex_q1 %c\n", word[counter]);
-      err();
-    }
-  }
-}
-
-void hex_q2(int counter, char word[TAM]) {
-  if(counter < TAM) {
-    if(word[counter] == '0') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '1') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '2') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '3') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '4') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '5') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '6') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '7') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '8') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == '9') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == 'a') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == 'b') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == 'c') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == 'd') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == 'e') {
-      hex_q2( ++counter, word);
-    }
-    else if(word[counter] == 'f') {
-      hex_q2( ++counter, word);
-    }
-    else if(!word[counter]) {
-      finish(word);
-    }
-    else {
-      if(DEBUG)
-        printf("hex_q2 %c\n", word[counter]);
-      err();
-    }
-  }
-}
-
-void err() {
-  NEXT = 1;
-}
-
-void finish(char word[TAM]) {
-  for(int i = 0; i < TAM; i++) {
-    printf("%c", word[i]);
+void print_word()
+{
+  for (int i = 0; i < TAM; i++)
+  {
+    printf("%c", WORD[i]);
   }
   printf("\n");
-  NEXT = 0;
 }
